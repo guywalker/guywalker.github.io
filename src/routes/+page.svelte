ee0993d0bec29a86c7d70ad7e6b5f49a7489fabd
Guy Walker <guy.w.walker@gmail.com>
2023-06-09 23:09:32 +0100

    nosleep WIP

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<img src="/images/dexter.jpg" alt="Dexter">
+
+<style>
+  img {
+    width: 100vw;
+    height: 100vh;
+    object-fit: cover;
+  }
+</style>
