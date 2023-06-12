8a25d403f5493f05e3439d231360c391a4359b71
Guy Walker <guy.w.walker@gmail.com>
2023-06-12 08:27:46 +0100

    Updates

--- _app/immutable/chunks/paths.75b9b019.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_k2b30f)==null?void 0:s.base)??"/sveltekit-github-pages";var e;const a=((e=globalThis.__sveltekit_k2b30f)==null?void 0:e.assets)??t;export{a,t as b};
